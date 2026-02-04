2ab0228d9a5e202aacc18f68f80dad30ab4f9b8b
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:32:04 +0530

    Add files via upload

---
+++ continuebreakpass1.py
@@ -0,0 +1,31 @@
+for i in range(1,11):
+    if i==6:
+        break
+    print(i)
+
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i)
+
+for i in range(1,6):
+    if i==4: 
+        continue
+    if i==5:
+        continue
+    print(i)
+
+for i in range(1,6):
+    if i==3:
+        pass
+    else:
+        print(i)
+
+for i in range(1,11):
+    if i==4: 
+        continue
+    if  i==5:
+        continue
+    if i==9:
+        break
+    print(i)
