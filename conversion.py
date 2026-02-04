904276f1b260787f49fcce394897c78c633fd03d
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:31:37 +0530

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,12 @@
+# Define a string and an integer
+string_value = "The number is "
+integer_value = 45
+
+#Convert the integer to a string using explicit conversion
+integer_as_string = str(integer_value)
+
+#Concatenate the string and the converted integer
+result = string_value + integer_as_string
+
+print(result)
+
