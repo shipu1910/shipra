7a1da87cc9a00720c7c43b3a56eef6385ac25586
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:25:10 +0530

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,43 @@
+# class Grandfather:
+#     def house(self):
+#         print("This is Grandfather house.")
+# class Father(Grandfather):
+#     def car(self):
+#         print("This is Father car.")
+# class Son(Father):
+#     def bike(self):
+#         print("This is son's bike.")
+# xyz= Son()
+# xyz.bike()
+# xyz.car()
+# xyz.house()  
+# abc= Father()
+# # abc.bike()
+# abc.car()
+# abc.house()
+
+# class NSTI:
+#     def campus(self):
+#         print("This is NSTI campus.")
+# class trade(NSTI):
+#     def teacher(self):
+#         print("There are 40 staffs.")
+# class student(trade):
+#     def student(self):
+#         print("There are 650 students.")
+# xyz=student()
+# xyz.teacher()
+# xyz.student()               
+# xyz.campus()
+# ab = trade()
+# ab.campus()
+# ab.teacher()
+
+# Multiple Inheritance
+class Mother:
+    def hair_color(self):
+        print("Brown")
+class Father:
+    def eye_color(self):
+        print("Blue")
+                
