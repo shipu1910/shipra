6a7555d31e634bbfaac1fad498d10be132b4d6dd
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:41:32 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,20 @@
+x=5
+y=6
+c=x+y
+print("The Sum of x & y is :",x+y )
+print("The Sum of",x,"and",y,"is :",c)#normal print statement
+print(f"The Sum of {x} and {y} is: {c}")#f string
+
+
+
+a=18
+b=20
+c=22
+x=a+b
+y=b+c
+z=c+a
+e=a+b+c
+print("The sum of a, b, c is :",e)
+print("The Sum of",a,b,c,"is :",e)
+print(f"The Sum of {a}, {b}, {c} is: {e}")
+print(f'Sum of a, b, c is :{e}')
