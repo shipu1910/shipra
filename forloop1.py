f007506fd5ccc1a5ff6bf8f88a3936e409cc3050
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:27:27 +0530

    Add files via upload

---
+++ forloop1.py
@@ -0,0 +1,25 @@
+name = "Shipra"
+for i in range(5):
+    print(name)
+
+for i in range(4,12):
+    print(i)    
+
+# num = 19
+# for i in range(1,11):
+#     print(num,"X",i,"=",num*i)
+# else:
+#     print("Table compleated.")
+
+# num = int(input("Enter your number :"))
+# for i in range(1,11):
+#     print(num,"X",i,"=",num*i)
+
+for i in range(1,5):
+    print("shipra" + str(i)) 
+
+for i in range(7):
+    print("*" *i) 
+
+for i in "Asmita":
+    print(i)                
