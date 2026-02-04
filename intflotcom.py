2052fd0cbc8727adcae4ac8c73680017f97e4b81
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:24:34 +0530

    Add files via upload

---
+++ intflotcom.py
@@ -0,0 +1,54 @@
+# Assigning integer literals to variables
+int1= 20
+int2= -5
+int3= 0
+
+print("Integer values :")
+print("int1 =", int1)
+print("int2 =", int2)
+print("int3 =", int3)
+
+#Performing operations on integer literals
+print("Sum of int1 and int2 :", int1 + int2)
+print("Product of int1 and 3 :", int1 * 3)
+print("Difference between int1 and int2 :", int1 - int2)
+
+
+# Assigning floating-point literals to variables
+float1= 3.14
+float2= -5.34
+float3= 0.0
+
+#Performing operations with floating-point literals
+sum_floats = float1 + float2
+product_floats = float1 * 2
+difference_floats = float1 - float2
+
+#Printing floating-point values and results of operations
+print("\nFloating-point values :")
+print("float1 =", float1)
+print("float2 =", float2)
+print("float3 =", float3)
+print("Sum of float1 and float2 :", sum_floats)
+print("Product of float1 and 2 :", product_floats)
+print("Difference between float1 and float2 :", difference_floats)
+
+
+# Assigning complex literals to variables
+com1= 5 + 6j
+com2= -4 - 7j
+com3= 0 + 0j
+
+#Performing operations with complex literals
+sum_complex = com1 + com2
+product_complex = com1 *(1 + 1j)
+difference_complex = com1 - com2
+
+#Printing complex values and results of operations
+print("\ncomplex values :")
+print("com1 =", com1)
+print("com2 =", com2)
+print("com3 =", com3)
+print("Sum of com1 and com2 :", sum_complex)
+print("Product of com1 and (1 +1j) :", product_complex)
+print("Difference between com1 and com2 :", difference_complex)
