5f1ddfd67877b782dde2b8e984ff5a4c40c075ab
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:30:18 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,25 @@
+name = "anu"
+age = "18"
+phone = 9458763565
+year = 2.5
+
+print(type(name))
+print(type(age))
+print(type(phone))
+print(type(year))
+
+print(name+age)
+age= int(18)
+
+
+x=10
+y=15
+print(x<y)
+print(x>y)
+
+
+x=10
+y="15"
+z=int(y)
+print(x+z)
+
