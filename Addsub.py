17c1586e77bb649bfdd619360e45a3296157bc78
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:34:17 +0530

    Add files via upload

---
+++ Addsub.py
@@ -0,0 +1,31 @@
+#Define two numbers
+a = 56
+b = 4
+
+# Addition
+add_result = a + b
+print("Addition: {} + {} = {}" .format(a, b, add_result))
+
+# Subtraction
+sub_result = a - b
+print("Subtraction: {} - {} = {}" .format(a, b, sub_result))
+
+# Multiplication
+multi_result = a * b
+print("Multiplication: {} * {} = {}" .format(a, b, multi_result))
+
+# Division
+div_result = a / b
+print("Division: {} / {} = {}" .format(a, b, div_result))
+
+# Floor Division
+floor_division_result = a // b
+print("Floor Division: {} // {} = {}" .format(a, b, floor_division_result))
+
+# Modulus
+mod_result = a % b
+print("Modulus: {} % {} = {}" .format(a, b, mod_result))
+
+# Exponentiation
+expon_result = a ** b
+print("Exponentiation: {} ** {} = {}" .format(a, b, expon_result))
