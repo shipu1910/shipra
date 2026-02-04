fd9f6b6b6ab0458405bbde2a562d81caaa7758c3
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:41:52 +0530

    Add files via upload

---
+++ userinput.py
@@ -0,0 +1,12 @@
+x=input("Enter your name :")
+print("Hello!", x)
+
+a=int(input("Enter your first number :" ))
+b=int(input("Enter your second number :" ))
+c = a+b
+print("The sum of two numbers is :", c)
+
+x= "Shivangi"
+y= "Singh"
+print("My full name is", x, "",y)
+print("My first name is", x, "and", "Last name is", y)
