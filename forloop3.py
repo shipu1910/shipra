12e4511860f721a0599cfd9300c79013e461de16
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:27:06 +0530

    Add files via upload

---
+++ forloop3.py
@@ -0,0 +1,29 @@
+# Print the multiplication table of a number entered by the user
+# num = int(input("Enter a number for multiplication table :"))
+# for i in range(1,11):
+#     print(num,"X",i,"=",num*i)
+
+# # # Print numbers in reverse order from 10 to 1
+# for i in range(10,0,-1):
+#     print(i)
+
+# # Find the sum of first 10 natural numbers
+# num = int(input("Enter a number :"))
+# for i in range(1, num+1):
+#     sum = i+i
+#     print(sum)
+
+# Print the factorial of a given number
+num = int(input("Enter a number to find its factorial: "))
+factorial = 1
+for i in range(1, num + 1):
+    factorial = factorial*i
+print("Factorial of", num, "is", factorial) 
+
+ 
+# Print each character of the word "Python" on a new line
+for char in "Python":
+    print(char)
+
+# total = sum(range(1,11))
+# print(total)
