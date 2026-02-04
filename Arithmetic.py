bd9a81f198d799736ddba2eec91300f41f9a5f96
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:33:22 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,22 @@
+print("The Sum of 56 & 60 :", 56+60)
+print("The Multiplication of 56 & 60 :", 56*60)
+print("The Division of 56 & 16 :", 56-16)
+print("The Subtraction of 56 & 12 :", 56/12)
+# f string (formatting string)
+
+print(f"sum of 50 & 20 is :{50+20}") 
+print(f"Multiplication of 50 & 20 is :{50*20}") 
+print(f"Division of 50 & 20 is :{50-20}") 
+print(f"Subtraction of 50 & 20 is :{50/20}") 
+
+
+# Arithmetic Operators
+x = 15
+y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y) # floot value
+print(x // y) # integer value
+print(x % y) # reminder value
+print(x ** y) 
