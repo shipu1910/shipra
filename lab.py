7da48b453273f8bb0f146969c19ec71fc827dbc8
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:23:42 +0530

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,25 @@
+print(f"The Result of {56} + {8} is : {56+8}")
+print(f"The Result of {56} - {8} is : {56-8}")
+print(f"The Result of {56} * {8} is : {56*8}")
+print(f"The Result of {56} / {8} is : {56/8}")
+print(f"The Result of {56} ** {8} is : {56**8}")
+print(f"The Result of {56} // {8} is : {56//8}")
+
+#Inserting space between outputs using commas
+print("The Sum of 56 & 60 is :", 56+60)
+print("The Product of 56 & 60 is :", 56*60)
+
+#custom separator between outputs
+print("5", "+", "12", "=", 5+12, sep=" ")
+print("5", "*", "12", "=", 5*12, sep=" ")
+
+#Inserting text at the end of a print statement using end parameter
+print("15 + 19 =", end=" ")
+print(15 + 19)
+print("15 * 19 =", end=" ")
+print(15 * 19)
+
+#Multi-line printing with multiple print statement
+print("Hello, World!")
+print("Welcome to Python programming.")
+print("This is multi-line printing.")
