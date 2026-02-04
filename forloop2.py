f2403c5a6a71824325fa56d5cffd624d0048a8f4
shipu1910 <shipra56yc788@gmail.com>
2026-02-04 13:28:25 +0530

    Add files via upload

---
+++ forloop2.py
@@ -0,0 +1,27 @@
+# Print numbers from 1 to 10
+for i in range(1,11):
+    print(i)
+
+# # Print your name 5 times
+for i in range(5):
+    print("Anamika Rai")
+
+# # Print all even numbers from 2 to 20
+for i in range(2,21,2):
+    print(i)
+
+# # Print all odd numbers from 1 to 19
+for i in range(1,20,2):
+    print(i)
+
+# # Print square of numbers from 1 to 10.(Example: 1², 2², 3², …)
+for i in range(1,11):
+    print(i,"2 =", i**2)
+
+num = int(input("Enter a number :"))
+for i in range(1, num+1):
+    sq = i*i
+    print(sq) 
+
+
+              
